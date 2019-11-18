bf7de462197e3c3960d4e87ccc1fb5f813506af8
Mandy Gang <mandygang@dhcp-rhodes-2097.redrover.cornell.edu>
2019-11-18 15:13:24 -0500

    edited db file

--- db.py
+++ db.py
@@ -6,94 +6,131 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-instructor_table = db.Table('instructors', db.Model.metadata,
-                             db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-                             db.Column('instructor_id', db.Integer, db.ForeignKey('user.id'))
+# user_table = db.Table('users', db.Model.metadata,
+#                     db.Column('entry_id', db.Integer, db.ForeignKey('entry.id')),
+#                     db.Column('goal_id', db.Integer, db.ForeignKey('goal.id'))
+#                     )
+
+tag_table = db.Table('tags', db.Model.metadata,
+                    db.Column('tag_id', db.Integer,db.ForeignKey('tag.id')),
+                    db.Column('entry_id', db.Integer,db.ForeignKey('entry.id'))
                     )
 
-student_table = db.Table('students', db.Model.metadata,
-                             db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-                             db.Column('student_id', db.Integer, db.ForeignKey('user.id'))
-                    )
+# instructor_table = db.Table('instructors', db.Model.metadata,
+#                              db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+#                              db.Column('instructor_id', db.Integer, db.ForeignKey('user.id'))
+#                     )
+#
+# student_table = db.Table('students', db.Model.metadata,
+#                              db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+#                              db.Column('student_id', db.Integer, db.ForeignKey('user.id'))
+                    # )
 
-class Course(db.Model):
-    __tablename__ = "course"
+# General User information
+class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String, nullable = False)
-    name = db.Column(db.String, nullable = False)
-    assignments = db.relationship('Assignment', cascade='delete')
-    instructors = db.relationship('User', secondary = instructor_table, back_populates='instructor_courses')
-    students = db.relationship('User', secondary = student_table, back_populates='student_courses')
+    firstName = db.Column(db.String, nullable = False)
+    lastName = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    phoneNum = db.Column(db.String, nullable = True)
+    # add table information for spending
+    entries = db.relationship('Entry', cascade='delete')
+    goals = db.relationship('Goal', cascade='delete')
 
     def __init__(self, **kwargs):
-        self.code = kwargs.get('code', '')
-        self.name = kwargs.get('name', '')
-        self.assignments = []
+        self.firstName = kwargs.get('firstName', '')
+        self.lastName = kwargs.get('lastName', '')
+        self.email = kwargs.get('email', '')
+        self.phoneNum = kwargs.get('phoneNum', '')
+        self.entries = []
+        self.goals = []
 
     def serialize(self):
         return {
             'id': self.id,
-            'code': self.code,
-            'name': self.name,
-            'assignments': [s.serialize() for s in self.assignments],
-            'instructors': [i.serialize2() for i in self.instructors],
-            'students': [s.serialize2() for s in self.students]
+            'firstName': self.firstName,
+            'lastName': self.lastName,
+            'email': self.email,
+            'phoneNum': self.phoneNum,
+            'entries': [s.serialize() for s in self.entries],
+            'goals': [s.serialize() for s in self.goals],
         }
-    def serialize2(self):
-        return {
-            'id': self.id,
-            'code': self.code,
-            'name': self.name
-        }
-    
-class Assignment(db.Model):
-    __tablename__ = 'assignment'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable = False)
-    due_date = db.Column(db.Integer, nullable = False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable = False)
-    
+
+# Spending entry either Expenses or Income
+# Need to link to the user
+class Entry(db.Model):
+    __tablename__ = 'entry'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)          # Title of the spending Entry
+    type = db.Column(db.String, nullable = False)           # Type of spending [EXPENSE, INCOME]
+    amount = db.Column(db.Float, nullable = False)          # The amount spent, Do we assume USD or BRB??
+    description = db.Column(db.String, nullable = True)     # Optional description of the entry
+    date = db.Column(db.Integer, nullable = False)          # Date the purchase was made (helps with display?)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    #tag = db.Column()                                 #IDK how to do this
+    tags = db.relationship('Tag', secondary = tag_table, back_populates='entries')
 
     def __init__(self, **kwargs):
         self.title = kwargs.get('title', '')
-        self.due_date = kwargs.get('due_date', '')
-        self.course_id = kwargs.get('course_id', '')
+        self.type = kwargs.get('type', '')
+        self.amount = kwargs.get('amount', '')
+        self.description = kwargs.get('description', '')
+        self.date = kwargs.get('date', '')
+        self.user_id = kwargs.get('user_id', '')
 
     def serialize(self):
         return {
             'id': self.id,
             'title': self.title,
-            'due_date': self.due_date
+            'type': self.type,
+            'amount': self.amount,
+            'description': self.description,
+            'date': self.date
         }
 
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable = False)
-    netid = db.Column(db.String, nullable = False)
-    flag = False
-    instructor_courses = db.relationship('Course', secondary=instructor_table, back_populates='instructors')
-    student_courses = db.relationship('Course', secondary=student_table, back_populates='students')
-    
+
+# Optional decision to include the tag in your spending entry
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key = True)
+    # name of the category [food, clothing, transport, entertainment, groceries, bills, miscellaneous, spending(Include for goal)]
+    name = db.Column(db.name, nullable = False)
+    entries = db.relationship('Entry', secondary = tag_table, back_populates='tags')
+    # Link to spending Entry
+
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', '')
-        self.netid = kwargs.get('netid', '')
-        
+
     def serialize(self):
-        courses = []
-        for c in self.instructor_courses:
-            courses.append(c.serialize2())
-        for c in self.student_courses:
-            courses.append(c.serialize2())
         return {
             'id': self.id,
-            'name': self.name,
-            'netid': self.netid,
-            'courses': courses
+            'name': self.name
         }
-    def serialize2(self):
-        return {
+
+
+# including the goal in your monthly spending tracking??
+class Goal(db.Model):
+    __tablename__ = 'goal'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    limit = db.Column(db.Integer, nullable = False)         #your limit for the month
+    length = db.Column(db.Integer, nullable = False)        #Default: Month; [week, month, year]
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    #link to goal to a specific tag, optional (could be for just the spending period)
+
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get('title', '')
+        self.limit = kwargs.get('limit', '')
+        self.length = kwargs.get('length', '')
+        self.user_id = kwargs.get('user_id', '')
+
+    def serialize(self):
+        return{
             'id': self.id,
-            'name': self.name,
-            'netid': self.netid
+            'title': self.title,
+            'limit': self.limit,
+            'length': self.length
         }
