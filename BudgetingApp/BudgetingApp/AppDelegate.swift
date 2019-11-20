240af9432f6131dad9675e65e602e84b1cf938f3
Janice Jung <yj249@cornell.edu>
2019-11-19 23:44:37 -0500

    Frontend added

---
+++ BudgetingApp/BudgetingApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BudgetingApp
+//
+//  Created by Janice Jung on 11/19/19.
+//  Copyright © 2019 Janice Jung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
